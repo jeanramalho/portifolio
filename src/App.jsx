f929aac5dd493596846173478d2102fc2e04f194
jeanramalho <jeanramalho.dev@gmail.com>
2022-01-19 17:45:17 -0300

    Cria Esqueleto de React Funcional para site

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from './components/Header/Header'
+
+function App() {
+    return ( 
+        <Header />
+    )
+}
+
+export default App
