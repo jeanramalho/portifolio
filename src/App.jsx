30b81155391e29207d467af5123c1453da7e82c8
jeanramalho <jeanramalho.dev@gmail.com>
2022-03-01 20:30:55 -0300

    Implenta card de visualizacao

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from './components/Header/Header'
 import Background from './components/Background/Background'
+import Card from './components/Card/Card'
 
 function App() {
     return ( 
         <fragment>
             <Header />
+            <Card />
             <Background />
         </fragment>
         
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Card.css"
+
+function Card() {
+    return(
+        <div className="card-box">
+            <div className="card-main">
+
+            </div>
+        </div>
+        
+    )
+}
+
+export default Card
