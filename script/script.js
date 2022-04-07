e00e9d62e4b2b9a8ece2536f28a6d436298ece05
jeanramalho <jeanramalho.dev@gmail.com>
2022-04-07 18:04:40 -0300

    Implementa botão de topo

--- script/script.js
+++ script/script.js
@@ -4,7 +4,15 @@ $(document).ready(function(){
             $('.navbar').addClass("sticky")
         }else{
             $('.navbar').removeClass("sticky")
+        }if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show")
+        }else{
+            $('.scroll-up-btn').removeClass("show")
         }
+    })
+
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
     })
 
     $('.menu-btn').click(function(){
